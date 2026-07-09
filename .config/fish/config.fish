fa2d2d5c217d2f94427f87fe114b44a11477e256
Kabab33 <kabab33@kabab33.nl>
2026-07-09 16:17:08 +0200

    modified:   .config/fish/config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -7,3 +7,27 @@ end
 
 # Created by `pipx` on 2026-01-07 08:31:13
 set PATH $PATH /home/koen/.local/bin
+
+function bind_bang
+    switch (commandline -t)[-1]
+        case "!"
+            commandline -t -- $history[1]
+            commandline -f repaint
+        case "*"
+            commandline -i !
+    end
+end
+
+function bind_dollar
+    switch (commandline -t)[-1]
+        case "!"
+            commandline -f backward-delete-char history-token-search-backward
+        case "*"
+            commandline -i '$'
+    end
+end
+
+function fish_user_key_bindings
+    bind ! bind_bang
+    bind '$' bind_dollar
+end
