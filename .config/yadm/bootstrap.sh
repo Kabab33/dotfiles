072549e38f12b8b3b5c557b8134ae03126202c4e
Kabab33 <kabab33@kabab33.nl>
2026-07-10 16:42:12 +0200

    modified:   ../.config/yadm/bootstrap.sh 	modified:   share/konsole/catppuccin-mocha.colorscheme

--- .config/yadm/bootstrap.sh
+++ .config/yadm/bootstrap.sh
@@ -3,25 +3,25 @@ echo "Haiii welkom bij de installer."
 echo "Ik ga hier effe sudo echo doen zodat je niet meer je wachtwoord hoeft in te voeren"
 sudo echo "Het werkt!"
 clear
-echo "                                                                     ";
-echo "                                                                     ";
-echo "   ,---,                         ___                 ,--,    ,--,    ";
-echo ",\`--.' |                       ,--.'|_             ,--.'|  ,--.'|    ";
-echo "|   :  :      ,---,            |  | :,'            |  | :  |  | :    ";
-echo ":   |  '  ,-+-. /  | .--.--.   :  : ' :            :  : '  :  : '    ";
-echo "|   :  | ,--.'|'   |/  /    '.;__,'  /    ,--.--.  |  ' |  |  ' |    ";
-echo "'   '  ;|   |  ,\"' |  :  /\`./|  |   |    /       \\ '  | |  '  | |    ";
-echo "|   |  ||   | /  | |  :  ;_  :__,'| :   .--.  .-. ||  | :  |  | :    ";
-echo "'   :  ;|   | |  | |\\  \\    \`. '  : |__  \\__\\/: . .'  : |__'  : |__  ";
-echo "|   |  '|   | |  |/  \`----.   \\|  | '.'| ,\" .--.; ||  | '.'|  | '.'| ";
-echo "'   :  ||   | |--'  /  /\`--'  /;  :    ;/  /  ,.  |;  :    ;  :    ; ";
-echo ";   |.' |   |/     '--'.     / |  ,   /;  :   .'   \\  ,   /|  ,   /  ";
-echo "'---'   '---'        \`--'---'   ---\`-' |  ,     .-./---\`-'  ---\`-'   ";
-echo "                                        \`--\`---'                     ";
-echo "                                                                     ";
+echo "                                                                     "
+echo "                                                                     "
+echo "   ,---,                         ___                 ,--,    ,--,    "
+echo ",\`--.' |                       ,--.'|_             ,--.'|  ,--.'|    "
+echo "|   :  :      ,---,            |  | :,'            |  | :  |  | :    "
+echo ":   |  '  ,-+-. /  | .--.--.   :  : ' :            :  : '  :  : '    "
+echo "|   :  | ,--.'|'   |/  /    '.;__,'  /    ,--.--.  |  ' |  |  ' |    "
+echo "'   '  ;|   |  ,\"' |  :  /\`./|  |   |    /       \\ '  | |  '  | |    "
+echo "|   |  ||   | /  | |  :  ;_  :__,'| :   .--.  .-. ||  | :  |  | :    "
+echo "'   :  ;|   | |  | |\\  \\    \`. '  : |__  \\__\\/: . .'  : |__'  : |__  "
+echo "|   |  '|   | |  |/  \`----.   \\|  | '.'| ,\" .--.; ||  | '.'|  | '.'| "
+echo "'   :  ||   | |--'  /  /\`--'  /;  :    ;/  /  ,.  |;  :    ;  :    ; "
+echo ";   |.' |   |/     '--'.     / |  ,   /;  :   .'   \\  ,   /|  ,   /  "
+echo "'---'   '---'        \`--'---'   ---\`-' |  ,     .-./---\`-'  ---\`-'   "
+echo "                                        \`--\`---'                     "
+echo "                                                                     "
 
-if command yay --version &> /dev/null; then
-    yay -Syu --noconfirm --needed \
+if command yay --version &>/dev/null; then
+  yay -Syu --noconfirm --needed \
     extra/nextcloud-client \
     extra/flatpak \
     extra/discover \
@@ -46,31 +46,33 @@ if command yay --version &> /dev/null; then
     aur/localsend-bin \
     extra/python-pipx \
     extra/python-typer \
-    aur/python-pick
+    aur/python-pick \
+    extra/code \
+    extra/wget
 else
-echo " _   _      _                   ";
-echo "| | | |    | |                  ";
-echo "| |_| | ___| | __ _  __ _ ___   ";
-echo "|  _  |/ _ \\ |/ _\` |/ _\` / __|  ";
-echo "| | | |  __/ | (_| | (_| \\__ \\_ ";
-echo "\\_| |_/\\___|_|\\__,_|\\__,_|___(_)";
-echo "                                ";
-echo "                                ";
-echo "🚫 Dit script werkt nog niet met jouw pakage manager."
-echo "   We supporten op dit moment alleen yay. Sorry!"
-exit
+  echo "Yay niet gevonden :("
+  echo "Dus ik ga yay installeren :)"
+
+  sudo pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay-bin.git && cd yay-bin && makepkg -si
+
+  echo "en de een twee daar gaan we weer opnieuw!"
+
+  yadm bootstrap
+
+  exit
+
 fi
 echo "🎉 Installatie van programmas is voltooid!"
 
 sleep 1
-echo "___  ____                _          __  __ ";
-echo "|  \\/  (_)              | |        / _|/ _|";
-echo "| .  . |_ ___  ___   ___| |_ _   _| |_| |_ ";
-echo "| |\\/| | / __|/ __| / __| __| | | |  _|  _|";
-echo "| |  | | \\__ \\ (__  \\__ \\ |_| |_| | | | |_ ";
-echo "\\_|  |_/_|___/\\___| |___/\\__|\\__,_|_| |_(_)";
-echo "                                           ";
-echo "                                           ";
+echo "___  ____                _          __  __ "
+echo "|  \\/  (_)              | |        / _|/ _|"
+echo "| .  . |_ ___  ___   ___| |_ _   _| |_| |_ "
+echo "| |\\/| | / __|/ __| / __| __| | | |  _|  _|"
+echo "| |  | | \\__ \\ (__  \\__ \\ |_| |_| | | | |_ "
+echo "\\_|  |_/_|___/\\___| |___/\\__|\\__,_|_| |_(_)"
+echo "                                           "
+echo "                                           "
 
 echo "🟦 Bluetooth instellen..."
 sudo systemctl enable bluetooth
@@ -79,7 +81,7 @@ echo "🟦 bluetooth klaar!"
 
 echo "📦 Flathub instellen.."
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
-echo "📦 Meer installeren met flatpak..."
+echo "📦 Dingen installeren met flatpak..."
 flatpak install -y --noninteractive --or-update org.gnome.World.Iotas
 
 echo "🐮 Broot instellen..."
@@ -100,26 +102,35 @@ echo "🐍 pipx autocompleet..."
 eval "$(register-python-argcomplete pipx)"
 fish -c "register-python-argcomplete --shell fish pipx >~/.config/fish/completions/pipx.fish"
 
-echo "🐍 Meer met pipx installeren"
+echo "🐍 Dingen met pipx installeren"
 pipx install snowmachine
 
+echo "🖌️ KDE Thema instellen..."
+git clone --depth=1 https://github.com/catppuccin/kde /tmp/bootstrap.d/theme/kde
+/tmp/bootstrap.d/theme/kde/install.sh 1 4 1 auto
+
+
+
+
 echo ""
-echo "                                                        ,---,  ";
-echo "       ,--.                                          ,\`--.' |  ";
-echo "   ,--/  /|  ,--,                                    |   :  :  ";
-echo ",---,': / ',--.'|                                    '   '  ;  ";
-echo ":   : '/ / |  | :                             __  ,-.|   |  |  ";
-echo "|   '   ,  :  : '                           ,' ,'/ /|'   :  ;  ";
-echo "'   |  /   |  ' |     ,--.--.      ,--.--.  '  | |' ||   |  '  ";
-echo "|   ;  ;   '  | |    /       \\    /       \\ |  |   ,''   :  |  ";
-echo ":   '   \\  |  | :   .--.  .-. |  .--.  .-. |'  :  /  ;   |  ;  ";
-echo "|   |    ' '  : |__  \\__\\/: . .   \\__\\/: . .|  | '   \`---'. |  ";
-echo "'   : |.  \\|  | '.'| ,\" .--.; |   ,\" .--.; |;  : |    \`--..\`;  ";
-echo "|   | '_\\.';  :    ;/  /  ,.  |  /  /  ,.  ||  , ;   .--,_     ";
-echo "'   : |    |  ,   /;  :   .'   \\;  :   .'   \\---'    |    |\`.  ";
-echo ";   |,'     ---\`-' |  ,     .-./|  ,     .-./        \`-- -\`, ; ";
-echo "'---'               \`--\`---'     \`--\`---'              '---\`\"  ";
-echo "                                                               ";
+
+echo ""
+echo "                                                        ,---,  "
+echo "       ,--.                                          ,\`--.' |  "
+echo "   ,--/  /|  ,--,                                    |   :  :  "
+echo ",---,': / ',--.'|                                    '   '  ;  "
+echo ":   : '/ / |  | :                             __  ,-.|   |  |  "
+echo "|   '   ,  :  : '                           ,' ,'/ /|'   :  ;  "
+echo "'   |  /   |  ' |     ,--.--.      ,--.--.  '  | |' ||   |  '  "
+echo "|   ;  ;   '  | |    /       \\    /       \\ |  |   ,''   :  |  "
+echo ":   '   \\  |  | :   .--.  .-. |  .--.  .-. |'  :  /  ;   |  ;  "
+echo "|   |    ' '  : |__  \\__\\/: . .   \\__\\/: . .|  | '   \`---'. |  "
+echo "'   : |.  \\|  | '.'| ,\" .--.; |   ,\" .--.; |;  : |    \`--..\`;  "
+echo "|   | '_\\.';  :    ;/  /  ,.  |  /  /  ,.  ||  , ;   .--,_     "
+echo "'   : |    |  ,   /;  :   .'   \\;  :   .'   \\---'    |    |\`.  "
+echo ";   |,'     ---\`-' |  ,     .-./|  ,     .-./        \`-- -\`, ; "
+echo "'---'               \`--\`---'     \`--\`---'              '---\`\"  "
+echo "                                                               "
 echo ""
 echo ""
 echo ""
